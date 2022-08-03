d65b3fa870821dd8e9d5f259fa0f29d05188cf68
mojotron <igormalj@gmail.com>
2022-08-03 14:23:27 +0200

    learn about useContext and useReducer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
-import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import "./styles/Navbar.css";
 import SearchBar from "./SearchBar";
-import { ThemeContext } from "../context/ThemeContext";
+import { useTheme } from "../hooks/useTheme";
 
 const Navbar = () => {
-  const { color } = useContext(ThemeContext);
+  const { color } = useTheme();
 
   return (
     <nav className="Navbar" style={{ backgroundColor: color }}>
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,8 +1,15 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const ThemeContext = createContext();
 
+const themeReducer = () => {};
+
 export const ThemeProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(themeReducer, {
+    color: "blue",
+  });
+
+  const changeColor = (color) => {};
   return (
     <ThemeContext.Provider value={{ color: "blue" }}>
       {children}
